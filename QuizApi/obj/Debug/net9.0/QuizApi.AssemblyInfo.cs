5bb32165f0f385abb78b5118a61bd83ebfeef1c8
faustasgudas <faustas.gudas@gmail.com>
2025-07-07 23:15:53 +0300

    Delete unnecessary startup project files/code

--- QuizApi/obj/Debug/net9.0/QuizApi.AssemblyInfo.cs
+++ QuizApi/obj/Debug/net9.0/QuizApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe4e5fb3bcca1ffdd13a99aa35e8525e8619936")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
