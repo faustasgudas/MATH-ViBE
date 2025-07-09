3129b197d44b1ecf6f134c5b4e7dc7b912f435e5
faustasgudas <faustas.gudas@gmail.com>
2025-07-09 21:09:27 +0300

    Add QuizAttempt model

---
+++ QuizApi/Models/QuizAttempt.cs
@@ -0,0 +1,15 @@
+namespace QuizApi.Models;
+
+public class QuizAttempt
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid UserId { get; set; }
+    public Guid QuizId { get; set; }
+    public decimal Score { get; set; }
+    public DateTime Started { get; set; } = DateTime.UtcNow;
+    public DateTime Completed { get; set; } = DateTime.UtcNow;
+    
+    public Quiz? Quiz { get; set; } = null!;
+    public User? User { get; set; } = null!;
+    
+}
