beaea0c73bd2182c4cd7a412d4d4bb9a1338f441
faustasgudas <faustas.gudas@gmail.com>
2025-07-09 21:01:18 +0300

    Add Rating model

---
+++ QuizApi/Models/Rating.cs
@@ -0,0 +1,14 @@
+namespace QuizApi.Models;
+
+public class Rating
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid UserId { get; set; }
+    public Guid QuizId { get; set; }
+    public int Stars { get; set; } = 0;
+    public string? Comment { get; set; } = null;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    public User? User { get; set; }
+    public Quiz? Quiz { get; set; }
+}
