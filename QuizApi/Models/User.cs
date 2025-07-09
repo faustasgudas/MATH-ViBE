79d301652da171ff9ad15e6b1750f25c3fa7fd55
faustasgudas <faustas.gudas@gmail.com>
2025-07-09 21:12:28 +0300

    Update User model with navigation properties

--- QuizApi/Models/User.cs
+++ QuizApi/Models/User.cs
@@ -9,4 +9,8 @@ public class User
     public string Role { get; set; } = "student";
     public DateTime Created { get; set; } =  DateTime.UtcNow;
     public int Xp { get; set; } = 0;
+    
+    public List<Quiz> CreatedQuizzes { get; set; } = new List<Quiz>();
+    public List<QuizAttempt> QuizAttempts { get; set; } = new();
+    public List<Rating> Ratings { get; set; } = new();
 }
